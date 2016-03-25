a3328c08e18f13c27f7391ee542b83409f378846
Elton <eltonmc@outlook.com>
2016-03-25 15:44:09 -0300

    Adicionando Fornecedor e ControleDeFornecedor

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeFornecedor.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeFornecedor.java
@@ -3,5 +3,41 @@ package ds2.equipe1.restaurante;
 /**
  * Created by Th on 24/03/2016.
  */
+
 public class ControleDeFornecedor {
+
+    public void cadastrarFornecedor(Fornecedor fornecedor){
+        //Banco de Dados
+        //SQL (Sem o Endereço)
+        //INSERT INTO fornecedor (nome, telefone, cnpj, email) VALUES (fornecedor.getNome(), fornecedor.getTelefone(), fornecedor.getCnpj(), fornecedor.getEmail());
+    }
+
+    public void alterarFornecedor(Fornecedor fornecedor){
+        //Banco de Dados
+        //SQL (Sem o Endereço)
+        //UPDATE fornecedor SET nome = fornecedor.getNome(), telefone = fornecedor.getNome(), email = fornecedor.getEmail WHERE cnpj = fornecedor.getCnpj();
+    }
+
+    public void excluirFornecedor(Fornecedor fornecedor){
+        //Banco de Dados
+        //SQL
+        //DELETE FROM fornecedor WHERE cnpj = fornecedor.getCnpj();
+    }
+
+    public void consultarFornecedor(Fornecedor fornecedor){
+        //Banco de Dados
+        //SQL
+        //SELECT id, nome, cnpj, telefone, email, id_endereco WHERE cnpj = funcionario.getCnpj() OR nome = funcionario.getNome();
+    }
+
+    public void relatorioFornecedor(Fornecedor fornecedor){
+        //Banco de dados
+        //Exibir Relatório
+    }
+
+    public void relatorioItensPorFornecedor(Item item, Fornecedor fornecedor){
+        //Banco de Dados
+        //Exibir Relatório
+    }
+
 }
--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/Fornecedor.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/Fornecedor.java
@@ -4,4 +4,49 @@ package ds2.equipe1.restaurante;
  * Created by Th on 24/03/2016.
  */
 public class Fornecedor {
+
+    private String nome;
+    private String telefone;
+    private String cnpj;
+    private String email;
+
+    public Fornecedor(String nome, String telefone, String cnpj, String email){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cnpj = cnpj;
+        this.email = email;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public String getTelefone (){
+        return telefone;
+    }
+
+    public String getCnpj(){
+        return cnpj;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setTelefone (String telefone){
+        this.telefone = telefone;
+    }
+
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
 }
