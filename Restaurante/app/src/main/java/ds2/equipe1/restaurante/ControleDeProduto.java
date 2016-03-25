7e0909269655bdf89ec8633b53622f6020998c09
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-25 12:30:59 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeProduto.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeProduto.java
@@ -4,4 +4,33 @@ package ds2.equipe1.restaurante;
  * Created by Th on 24/03/2016.
  */
 public class ControleDeProduto {
+	
+	public ControleDeProduto(){
+
+    }
+    
+    public void cadastrarProduto(String nome, float preco, int id, ArrayList <Item> itensNecessarios){
+
+    }
+    
+    public void excluirProduto(){
+        
+    }
+    
+    public void alterarProduto(){
+        
+    }
+    
+    public void consultarProduto(){
+        
+    }
+    
+	public void relatorioProdutos(){
+        
+    }
+    
+	public void relatorioItensPorProduto(){
+        
+    }
+	
 }
