a354ff0ecb469f3a78c3b51328f5d145f4359d70
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-25 12:21:50 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/Produto.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/Produto.java
@@ -5,10 +5,56 @@ package ds2.equipe1.restaurante;
  */
 public class Produto {
 
-    float preco = 0;
+	private String nome;
+    private float preco;
+    private int id;
+    private ArrayList <Item> itens = new ArrayList <Item>();
+    
+	public Produto(String nome, float preco, int id, ArrayList <Item> itens){
+		this.nome = nome;
+		this.preco = preco;
+		this.id = id;
+		this.itens = itens;
+    }
+    
+	public boolean validarProduto(){
+		for (int i = 0; i < itens.size(); i++) {
+            if (itens.get(i).getQuantidade() < 0) {
+				return false;
+			}
+        }
+        return true;
+    }
+    
+	public String getNome(){
+        return nome;
+    }
 
     public float getPreco(){
-
         return preco;
+    }
+    
+	public int getId(){
+        return id;
+    }
+    
+    public ArrayList <Item> getItens(){
+        return itens;
+    }
+    
+	public void setNome(String nome){
+		this.nome = nome;
+    }
+    
+    public void setPreco(float preco){
+		this.preco = preco;
+    }
+    
+    public void setId(int id){
+		this.id = id;
+    }
+    
+    public void setItens(ArrayList <Item> itens){
+		this.itens = itens;
     }
 }
