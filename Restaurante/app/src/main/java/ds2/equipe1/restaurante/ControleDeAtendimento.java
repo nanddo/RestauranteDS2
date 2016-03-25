15670f22dd08fc899a9c3cbb056b38960e10e272
Thales Francisco <thales@alvesnet.net>
2016-03-25 11:31:41 -0300

    Falta só métodos de relatório
    Com consulta ao banco e tal

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeAtendimento.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeAtendimento.java
@@ -23,9 +23,62 @@ public class ControleDeAtendimento {
         ArrayList< Produto > listaDeProdutos = pedido.getProdutos();
         for(int i = 0; i < listaDeProdutos.size(); i++){
             System.out.println(listaDeProdutos.get(i).toString());
+            //substituir essa função por impressão na impressora da cozinha
         }
     }
 
-    
+    public void imprimirConta(Comanda comanda){
+        ArrayList < Pedido > listaDePedidos = comanda.getPedidos();
+        for(int i = 0; i < listaDePedidos.size(); i++) {
+            ArrayList < Produto > listaDeProdutos = listaDePedidos.get(i).getProdutos();
+            for(int j = 0; j < listaDeProdutos.size(); j++) {
+                //aqui vai imprimir num pdf, num papelzinho, numa impressora térmica
+                //qqr coisa dessas aí
+                System.out.println(listaDeProdutos.get(i).toString() + " " + listaDeProdutos.get(i).getPreco());
+            }
+        }
+        float total = comanda.getTotal();
+        System.out.println("Total de Pedidos" + total + "\n 10% = " + 0.1*total + "\n Total " + 1.1*total);
+    }
+
+    public ArrayList < Produto > consultarPedido(Pedido pedido){
+        return pedido.getProdutos();
+    }
+
+    public void incluirPedido(Comanda comanda, Pedido pedido){
+        comanda.addPedido(pedido);
+    }
+
+    public void entregarPedido(Pedido pedido){
+        pedido.setEntregue();
+    }
+
+    public void cancelarPedido(Comanda comanda, Pedido pedido) {
+        comanda.removerPedido(pedido);
+    }
+
+    public ArrayList < Produto > consultarCardapio() {
+        ArrayList < Produto > cardapio = new ArrayList < Produto >();
+        //Aqui só com consulta ao DB né? pq precisa pegar a lista de produtos.
+        //fica pra outra hora beijo no coração!
+        return cardapio;
+    }
+
+    public ArrayList < Comanda > relatorioComandasAtivas(){
+        ArrayList < Comanda > ativas = new ArrayList < Comanda >();
+        //preciso de consulta ao banco neste ponto
+        return ativas;
+    }
+
+
+    public ArrayList < Comanda > relatorioComandasEncerradas(){
+        ArrayList < Comanda > encerradas = new ArrayList < Comanda >();
+        //preciso de consulta ao banco neste ponto
+        return encerradas;
+    }
+
+    public void relatorioArrecadacaoBruta(){
+        //preciso de consulta ao banco neste ponto
+    }
 
 }
