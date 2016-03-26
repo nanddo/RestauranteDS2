bfb5246140bc2885ad062853525e506edd93b1d5
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-26 10:59:51 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeItem.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeItem.java
@@ -4,4 +4,15 @@ package ds2.equipe1.restaurante;
  * Created by Th on 24/03/2016.
  */
 public class ControleDeItem {
+	
+	private ArrayList <Item> itens = new ArrayList <Item>();
+	
+	public Item consultarItem (String nome){
+		for (int i = 0; i < itens.size(); i++) {
+            if (itens.get(i).getNome() == nome) {
+				return itens.get(i);
+			}
+        }
+        return null;
+	}
 }
