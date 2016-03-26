f26ea4bfc83afbcffac99313e6c439ae37bdf202
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-26 11:21:39 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/Item.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/Item.java
@@ -8,42 +8,65 @@ public class Item {
 	private String nome;
 	private int quantidade;
 	private int limiteMinimo;
+	private boolean emUso;
 	
 	public Item(String nome, int quantidade, int limiteMinimo){
 		this.nome = nome;
 		this.quantidade = quantidade;
 		this.limiteMinimo = limiteMinimo;
-  }
+		emUso = false;
+    }
     
-  public boolean AlertarItemAbaixoDoLimite(){
-    if (quantidade < limiteMinimo) {
+    public Item (String nome){
+		//criar objeto baseado nos dados do banco
+	}
+    
+    public boolean AlertarItemAbaixoDoLimite(){
+        if (quantidade < limiteMinimo) {
 			return true;
 		} else {
 			return false;
 		}
-  }
+    }
     
 	public String getNome(){
-    return nome;
-  }
+        return nome;
+    }
 
-  public int getQuantidade(){
-    return quantidade;
-  }
+    public int getQuantidade(){
+        return quantidade;
+    }
     
 	public int getLimiteMinimo(){
-    return limiteMinimo;
-  }
+        return limiteMinimo;
+    }
     
+    /* Nome deve ser imutavel
 	public void setNome(String nome){
 		this.nome = nome;
-  }
+    }
+    */
     
-  public void setQuantidade(int quantidade){
-    this.quantidade = quantidade;
-  }
+    public void setQuantidade(int quantidade){
+		//alterar quantidade no banco
+		this.quantidade = quantidade;
+    }
     
-  public void setLimiteMinimo(int limiteMinimo){
+    public void setLimiteMinimo(int limiteMinimo){
+		//alterar limiteMinimo no banco
 		this.limiteMinimo = limiteMinimo;
-  }
+    }
+    
+    public boolean solicitarUso(){
+		if (emUso) {
+			return false;
+		} else {
+			emUso = true;
+			return true;
+		}
+	}
+	
+	public void liberarUso(){
+		emUso = false;
+	}
 }
