0be9299a1b8e555b9fc5dd9a6785638e44a397ac
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-26 11:21:23 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/Produto.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/Produto.java
@@ -7,19 +7,24 @@ public class Produto {
 
 	private String nome;
     private float preco;
-    private int id;
-    private ArrayList <Item> itens = new ArrayList <Item>();
+    //private int id;
+    private ArrayList <Item> ingredientes;
+    private boolean emUso;
     
-	public Produto(String nome, float preco, int id, ArrayList <Item> itens){
+	public Produto(String nome, float preco, /*int id,*/ ArrayList <Item> ingredientes){
 		this.nome = nome;
 		this.preco = preco;
-		this.id = id;
-		this.itens = itens;
+		//this.id = id;
+		this.ingredientes = ingredientes;
     }
     
+    public Produto(String nome){
+		//criar objeto baseado nos dados do banco
+	}
+    
 	public boolean validarProduto(){
-		for (int i = 0; i < itens.size(); i++) {
-            if (itens.get(i).getQuantidade() < 0) {
+		for (int i = 0; i < ingredientes.size(); i++) {
+            if (ingredientes.get(i).getQuantidade() > ControleDeItem.consultarItem(ingredientes.get(i).getNome()).getQuantidade()) {
 				return false;
 			}
         }
@@ -34,27 +39,48 @@ public class Produto {
         return preco;
     }
     
+    /*
 	public int getId(){
         return id;
     }
+    */
     
     public ArrayList <Item> getItens(){
         return itens;
     }
     
+    /* Nome deve ser imutavel
 	public void setNome(String nome){
 		this.nome = nome;
     }
+    */
     
     public void setPreco(float preco){
 		this.preco = preco;
     }
     
+    /*
     public void setId(int id){
 		this.id = id;
     }
+    */
     
+    /*
     public void setItens(ArrayList <Item> itens){
 		this.itens = itens;
     }
+    */
+    
+	public boolean solicitarUso(){
+		if (emUso) {
+			return false;
+		} else {
+			emUso = true;
+			return true;
+		}
+	}
+	
+	public void liberarUso(){
+		emUso = false;
+	}
 }
