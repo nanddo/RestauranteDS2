8198d046ce2cc2f3a4043224dab9c4046da37286
Rodrigo Benedito Otoni <rodrigo7491@gmail.com>
2016-03-26 11:21:54 -0300

    update

--- Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeProduto.java
+++ Restaurante/app/src/main/java/ds2/equipe1/restaurante/ControleDeProduto.java
@@ -3,34 +3,60 @@ package ds2.equipe1.restaurante;
 /**
  * Created by Th on 24/03/2016.
  */
-public class ControleDeProduto {
+public static class ControleDeProduto {
+	
+	private ArrayList <Item> produtos;
 	
 	public ControleDeProduto(){
-
+		//classe estatica tem construtor?
     }
     
-    public void cadastrarProduto(String nome, float preco, int id, ArrayList <Item> itensNecessarios){
-
+    public void carregarProdutosDoBanco(){
+		produtos = new ArrayList <Item>();
+		int tamanhoDoCardapio = 5; //Ler o tamanho do banco
+		
+		for (int i = 0; i < tamanhoDoCardapio; i++) {
+			String nome = "teste"; //Ler o nome do banco
+            produtos.add(new Produto(nome));
+        }
+	}
+    
+    public void cadastrarProduto(String nome, float preco, /*int id,*/ ArrayList <Item> ingredientes){
+		produtos.add(new Produto(nome, preco, ingredientes);
     }
     
-    public void excluirProduto(){
-        
+    public void excluirProduto(String nome){
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getNome() == nome) {
+				//Remover do banco
+				produtos.remove(i); //ver se e este mesmo o metodo de remocao
+			}
+        }
     }
     
-    public void alterarProduto(){
-        
+    public void alterarPrecoDeProduto(String nome, float novoPreco){
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getNome() == nome) {
+				//Alterar preco no banco
+				produtos.get(i).setPreco(novoPreco);
+			}
+        }
     }
     
-    public void consultarProduto(){
-        
+    public Produto consultarProduto(String nome){
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getNome() == nome) {
+				return produtos.get(i);
+			}
+        }
     }
     
 	public void relatorioProdutos(){
-        
+        //Gerar pdf?
     }
     
 	public void relatorioItensPorProduto(){
-        
+        //Gerar pdf?
     }
 	
 }
